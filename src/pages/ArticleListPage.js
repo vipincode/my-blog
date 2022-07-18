216924132cd246d6a19812349e1498f85cd39083
Vipin Singh <svipin174@gmail.com>
2022-07-18 23:55:58 +0530

    add reusable code to filter data

--- src/pages/ArticleListPage.js
+++ src/pages/ArticleListPage.js
@@ -1,16 +1,11 @@
-import { Link } from 'react-router-dom';
+import ArticleList from '../components/ArticleList';
 import articleContent from './article-content';
 
 const ArticleListPage = () => {
   return (
     <>
       <h1>Articles</h1>
-      {articleContent.map((article, key) => (
-        <Link key={key} to={`/article/${article.name}`}>
-          <h3>{article.title}</h3>
-          <p>{article.content[0].substring(0, 150)}...</p>
-        </Link>
-      ))}
+      <ArticleList articles={articleContent} />
     </>
   );
 };
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom';
+import ArticleList from '../components/ArticleList';
 import articleContent from './article-content';
 
 const ArticlePage = () => {
@@ -9,12 +10,18 @@ const ArticlePage = () => {
 
   if (!article) return <h1>Article does not exist!</h1>;
 
+  const otherArticles = articleContent.filter(
+    (article) => article.name !== name
+  );
+
   return (
     <>
       <h1>{article.title}</h1>
       {article.content.map((paragraph, key) => {
         return <p key={key}>{paragraph}</p>;
       })}
+      <h3>Other Articles:</h3>
+      <ArticleList articles={otherArticles} />
     </>
   );
 };
